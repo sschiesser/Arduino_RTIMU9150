253db9dcb4d0f36cdcf05d5f7224a33e52c078ec
Sébastien Schiesser <sebastien.schiesser@zhdk.ch>
2015-04-11 17:43:23 +0200

    Started calibration routine with PCInterrupt or 'c' / 's'... debouncing, resp. timing issues

--- ArduinoIMU/ArduinoIMU.ino
+++ ArduinoIMU/ArduinoIMU.ino
@@ -27,8 +27,12 @@
 #include "RTIMU.h"
 #include "RTFusionRTQF.h"
 #include "CalLib.h"
-#include <EEPROM.h>x
+#include <EEPROM.h>
 #include <PinChangeInt.h>
+
+#define LED_OFF          0
+#define LED_WORKING      1
+#define LED_CALIBRATING  2
 
 /* ****************************************************************
    ***                       GENERAL                            ***
@@ -36,8 +40,13 @@
 // debug flag for terminal monitoring
 boolean debug = false;
 
+// Compass calibration button
+uint8_t compassCalibPin = 2; // Digital in 2 (PCint)
+boolean calibMode = false;
+int calibDebounceDelay = 3000;
+
 // LED blink mode
-boolean blinkMode = true;
+uint8_t blinkMode = LED_WORKING;
 boolean blinkState = false;
 uint8_t ledPin = 13;
 
@@ -54,7 +63,7 @@ uint8_t transmit[60] = {60, 191,   0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,    0,0,0,
    ****************************************************************/
 // thumbJoy pinout (L to R):
 // Xout - 5V - Yout - gnd
-boolean thumbJoy = false;
+boolean thumbJoy = true;
 uint8_t thumbPinX = 0; // Analog read A0 pin
 uint8_t thumbPinY = 1; // Analog read A1 pin
 uint8_t thumbButtonPin = 12; // Digital in 12 (PCint)
@@ -88,8 +97,8 @@ uint8_t tbButtonPin = 7; // Digital in 7 (PCInt)
 uint8_t tbTransmitPos = 47; // First position of the trackball values in the transmit array
 uint8_t tbVals = 5; // Number of positions occupied by trackball in the transmit array
 
-volatile uint8_t tbWheelVertCntRaw = 120;
-volatile uint8_t tbWheelHorizCntRaw = 120;
+volatile uint8_t tbWheelVertCnt = 120;
+volatile uint8_t tbWheelHorizCnt = 120;
 uint8_t tbWheelCntMax = 240;
 uint8_t tbWheelCntMin = 0;
 uint8_t tbWheelIncStep = 10;
@@ -102,6 +111,7 @@ boolean tbButtonPressed = false;
 RTIMU *imu;                                           // the IMU object
 RTFusionRTQF fusion;                                  // the fusion object
 RTIMUSettings settings;                               // the settings object
+CALLIB_DATA calData;                                  // the calibration data
 
 // GYRO_BIAS_RATE sets the rate at which gyro bias is checked
 #define GYRO_BIAS_RATE 2000                           // interval between gyro bias check
@@ -154,6 +164,10 @@ void setup()
     Serial.begin(SERIAL_PORT_SPEED);
     Wire.begin();
     pinMode(ledPin, OUTPUT);
+    
+    /* Pin change interrupt to enter compass calibration mode */
+    pinMode(compassCalibPin, INPUT_PULLUP);
+    PCintPort::attachInterrupt(compassCalibPin, &compassCalibInt, FALLING);
     
     /* IMU setup
      * default setting values: "../libraries/RTIMULib/RTIMUSettings.cpp"
@@ -235,10 +249,7 @@ void loop()
       gyroData = imu->getGyro();          // get gyro data
       tsData = imu->getTimestamp();       // get calculated timestamp
       quatData = fusion.getFusionQPose(); // get fused quaternions
-      if(debug) {
-        Serial.print("Got data...\nNOW: "); Serial.println(now);
-        Serial.print("TSdata: "); Serial.println(tsData);
-      }
+
       cookIMU(now);
     }
     
@@ -255,8 +266,26 @@ void loop()
       transmit[thumbTransmitPos+4] = (thumbButtonPressed) ? 1 : 0;
     }
     
-
-    Serial.write(transmit, 60);
+    if(trackball) {
+      if(debug) {
+        Serial.print("tbWheel: "); Serial.print(tbWheelHorizCnt); Serial.print(" "); Serial.println(tbWheelVertCnt);
+      }
+      transmit[tbTransmitPos] = (uint8_t)((tbWheelHorizCnt >> 8) & 0xFF);
+      transmit[tbTransmitPos+1] = (uint8_t)(tbWheelHorizCnt & 0xFF);
+      transmit[tbTransmitPos+2] = (uint8_t)((tbWheelVertCnt >> 8) & 0xFF);
+      transmit[tbTransmitPos+3] = (uint8_t)(tbWheelVertCnt & 0xFF);
+      transmit[tbTransmitPos+4] = (tbButtonPressed) ? 1 : 0;
+    }
+    
+    if(Serial.available()) {
+      if(Serial.read() == 'c') {
+        compassCalibEnter();
+      }
+    }
+    
+    if(!debug) {
+//      Serial.write(transmit, 60);
+    }
 }
 
 
@@ -267,7 +296,7 @@ void cookIMU(unsigned long ts) {
   unsigned long delta = tsData - lastSample;
 
   if(debug) {
-    Serial.print("Cooking...\nts: "); Serial.print(ts, DEC);
+//    Serial.print("Cooking...\nts: "); Serial.print(ts, DEC);
     Serial.print(" tsData: "); Serial.print(tsData, DEC);
     Serial.print(" lastSample: "); Serial.print(lastSample, DEC);
     Serial.print(" delta: "); Serial.println(delta, DEC);
@@ -365,7 +394,7 @@ void cookIMU(unsigned long ts) {
     }
   }
   
-  if(blinkMode) {
+  if(blinkMode == LED_WORKING) {
     blinkState = !blinkState;
     digitalWrite(ledPin, blinkState);
   }
@@ -373,8 +402,104 @@ void cookIMU(unsigned long ts) {
 
 
 // ================================================================
+// ===                   CALIBRATION ROUTINES                   ===
+// ================================================================
+// Entering calibration...
+void compassCalibEnter() {
+  calibMode = true;
+  
+  calLibRead(0, &calData);                           // pick up existing mag data if there   
+  calData.magValid = false;
+  for (int i = 0; i < 3; i++) {
+    calData.magMin[i] = 10000000;                    // init mag cal data
+    calData.magMax[i] = -10000000;
+  }
+  
+  if(debug) {
+    Serial.println("Starting compass calibration");
+    Serial.println("Enter s or press button again to save current data to EEPROM");
+  }
+  
+  imu->IMUInit();
+  imu->setCalibrationMode(true);                    // make sure we get raw data
+  if(debug) {
+    Serial.print("Calibrating device "); Serial.println(imu->IMUName());
+  }
+
+  compassCalibrate();
+}
+
+// Exiting calibration...
+void compassCalibExit() {
+  calibMode = false;
+  calData.magValid = true;
+  calLibWrite(0, &calData);
+  if(debug) {
+    Serial.println("Exiting calibration mode...");
+    Serial.print("Data saved for device "); Serial.println(imu->IMUName());
+  }
+}
+
+// Calibrating...
+void compassCalibrate() {
+  boolean changed;
+  RTVector3 mag;
+  
+  while(calibMode) {
+    if(imu->IMURead()) {
+      changed = false;
+      mag = imu->getCompass();
+      for(int i = 0; i < 3; i++) {
+        if(mag.data(i) < calData.magMin[i]) {
+          calData.magMin[i] = mag.data(i);
+          changed = true;
+        }
+        if(mag.data(i) > calData.magMax[i]) {
+          calData.magMax[i] = mag.data(i);
+          changed = true;
+        }
+      }
+      
+      if(changed && debug) {
+        Serial.println("--------");
+        Serial.print("minX: "); Serial.print(calData.magMin[0]);
+        Serial.print(" maxX: "); Serial.print(calData.magMax[0]); Serial.println();
+        Serial.print("minY: "); Serial.print(calData.magMin[1]);
+        Serial.print(" maxY: "); Serial.print(calData.magMax[1]); Serial.println();
+        Serial.print("minZ: "); Serial.print(calData.magMin[2]);
+        Serial.print(" maxZ: "); Serial.print(calData.magMax[2]); Serial.println();
+      }
+    }
+    
+    if(Serial.available()) {
+      if(Serial.read() == 's') {
+        compassCalibExit();
+      }
+    }
+  }
+}
+
+
+// ================================================================
 // ===                    INTERRUPT ROUTINES                    ===
 // ================================================================
+void compassCalibInt() {
+  volatile int lastDebounceTime = millis();
+  Serial.println("Calib switch");
+  
+  while((millis() - lastDebounceTime) < calibDebounceDelay);;
+  
+  if(debug) Serial.print("Debounce time done... calib mode: "); Serial.println((calibMode) ? ("ON") : ("OFF"));
+  
+  if(digitalRead(compassCalibPin) == 0) {
+    if(calibMode) {
+      compassCalibExit();
+    } else {
+      compassCalibEnter();
+    }
+  }
+}
+
 void tbButtonInt() {
   if(digitalRead(tbButtonPin) == LOW) {
     digitalWrite(tbLedWhitePin, HIGH);
@@ -389,25 +514,25 @@ void tbButtonInt() {
 
 void tbWheelUpInt() {
   digitalWrite(tbLedBluePin, HIGH);
-  if(tbWheelVertCntRaw < tbWheelCntMax) tbWheelVertCntRaw += tbWheelIncStep;
+  if(tbWheelVertCnt < tbWheelCntMax) tbWheelVertCnt += tbWheelIncStep;
   if(debug) Serial.println("tb wheel UP");
 }
 
 void tbWheelDownInt() {
   digitalWrite(tbLedBluePin, LOW);
-  if(tbWheelVertCntRaw > tbWheelCntMin) tbWheelVertCntRaw -= tbWheelIncStep;
+  if(tbWheelVertCnt > tbWheelCntMin) tbWheelVertCnt -= tbWheelIncStep;
   if(debug) Serial.println("tb wheel DOWN");
 }
 
 void tbWheelLeftInt() {
   digitalWrite(tbLedRedPin, HIGH);
-  if(tbWheelHorizCntRaw < tbWheelCntMax) tbWheelHorizCntRaw += tbWheelIncStep;
+  if(tbWheelHorizCnt < tbWheelCntMax) tbWheelHorizCnt += tbWheelIncStep;
   if(debug) Serial.println("tb wheel LEFT");
 }
 
 void tbWheelRightInt() {
   digitalWrite(tbLedRedPin, LOW);
-  if(tbWheelHorizCntRaw > tbWheelCntMin) tbWheelHorizCntRaw -= tbWheelIncStep;
+  if(tbWheelHorizCnt > tbWheelCntMin) tbWheelHorizCnt -= tbWheelIncStep;
   if(debug) Serial.println("tb wheel RIGHT");
 }
 
